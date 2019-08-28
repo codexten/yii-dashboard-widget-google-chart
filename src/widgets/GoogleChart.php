000a9737196f87e875d106e505cf4802c9ad1ecf
Jomon Johnson <jomon.entero@gmail.com>
2019-08-28 13:43:02 +0530

    init

---
+++ src/widgets/GoogleChart.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace codexten\yii\modules\dashboard\widgets\GoogleChart\widgets;
+
+use codexten\yii\modules\dashboard\widgets\DashboardWidget;
+
+class GoogleChart extends DashboardWidget
+{
+
+}
